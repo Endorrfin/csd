9c9be683d4b60b425d5caaed6b9f47fc548386ad
Krupka <krupka.ua@gmail.com>
2026-03-11 11:58:00 +0200

    csd | web-portal: backend - adding blog section

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ContentModule } from './modules/content/content.module';
+import { BlogModule } from './modules/blog/blog.module';
 
 @Module({
   imports: [
@@ -33,6 +34,7 @@ import { ContentModule } from './modules/content/content.module';
       UsersModule,
       AuthModule,
       ContentModule,
+      BlogModule,
   ],
   controllers: [AppController],
   providers: [AppService],
